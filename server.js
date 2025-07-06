332ed553985138fab0975790e3a0f80197dcd98f
8 bit <cheshireterminal@gmail.com>
2025-07-06 00:43:23 -0400

    fff

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 
 const server = http.createServer((req, res) => {
     let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
